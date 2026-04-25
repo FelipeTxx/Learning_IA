b19fba0263214defb9fedb2b5e521d12344291ff
FelipeTxt <feliperemes1@gmail.com>
2026-04-25 15:40:55 -0300

    0.3

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ mp_drawing = mp.solutions.drawing_utils
 
 
 
-
+img = cv2.imread("imagem.jpg")
 while True:
     ret, frame = camera.read()
     
@@ -23,7 +23,7 @@ while True:
         mp_pose.POSE_CONNECTIONS
         
     )
-    cv2.imshow("Cam", frame)
+    #cv2.imshow("Cam", frame)
     if resultado.pose_landmarks:
         pontos = resultado.pose_landmarks.landmark
         nariz = pontos[mp_pose.PoseLandmark.NOSE]
@@ -33,7 +33,12 @@ while True:
         joelho_direito = pontos[mp_pose.PoseLandmark.RIGHT_KNEE]
 
 
-        analisar_postura(nariz, quadril_esquerdo, quadril_direito, joelho_esquerdo, joelho_direito)       
+        postura_analisada = analisar_postura(nariz, quadril_esquerdo, quadril_direito, joelho_esquerdo, joelho_direito)   
+        
+        cv2.putText(frame, postura_analisada[-1], (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+       #print(postura_analisada)
+        cv2.imshow("Tela", frame)
+            
     
 
     if cv2.waitKey(1) == 27:
--- rules.py
+++ rules.py
@@ -1,53 +1,77 @@
+from types import SimpleNamespace
+import math
 estado = ["Iniciando!"]
 tempo_validador = 60
+tempo = 15
 primeiro_quadro = True
+
+
+
 def analisar_postura(nariz, quadril_esquerdo, quadril_direito, joelho_esquerdo, joelho_direito):
     global estado, tempo_validador, primeiro_quadro 
-    #Verificar se esta deitado baseando-se no quadril direito
-    if nariz.y >= quadril_direito.y-0.2 and nariz.y <= quadril_direito.y+0.2 and not(nariz.x >= quadril_direito.x-0.2 and nariz.x <= quadril_direito.x+0.2):       
+    if len(estado) >= 30:
+        estado.pop(0)
+    centro_quadril = SimpleNamespace(
+        y = (quadril_esquerdo.y + quadril_direito.y)/2,
+        x = (quadril_esquerdo.x + quadril_direito.x)/2
+    ) 
+    centro_joelho = SimpleNamespace(
+         y = (joelho_esquerdo.y + joelho_direito.y)/2,
+         x = (joelho_esquerdo.x + joelho_direito.x)/2
+    ) 
+    #diferença de x entre nariz e quadril
+    dx_narizQuadril = abs(nariz.x - centro_quadril.x)
+    dy_narizQuadril = abs(nariz.y - centro_quadril.y)
+    #diferença de quadril e joelho
+    dx_quadrilJoelho = abs(centro_quadril.x - centro_joelho.x)
+    dy_quadrilJoelho = abs(centro_quadril.y - centro_joelho.y)
+    #angulo entre nariz e quadril
+    anguloG = math.degrees(math.atan2(dy_narizQuadril, dx_narizQuadril))
+    #angulo entre joelho e quadril
+    anguloJQ = math.degrees(math.atan2(dy_quadrilJoelho, dx_quadrilJoelho))
+
+    #print(anguloJQ)
+
+    #Verificar se esta deitado baseando-se no angulo geral do corpo
+    if anguloG <= 50:       
         if primeiro_quadro:
-            tempo_validador = 60 
+            tempo_validador = tempo 
             primeiro_quadro = False       
         tempo_validador-=1
         if tempo_validador <= 0:
+            if estado[-1] != "deitado":
+                print(estado[-1])
             estado.append("deitado")
-            print(estado[-1])            
-            tempo_validador = 60 
+            tempo_validador = tempo 
             primeiro_quadro = True           
-            return "deitado"
-            
+            return estado
+    #Verificar se esta em pé baseando-se no anguloG
     
-    '''#Verificar se esta deitado baseando-se no quadril esquerdo
-    elif nariz.y >= quadril_esquerdo.y-0.2 and nariz.y <= quadril_esquerdo.y+0.2 and not(nariz.x >= quadril_esquerdo.x-0.2 and nariz.x <= quadril_esquerdo.x+0.2) and estado[-1] != "deitado":
-        estado.append("deitado")
-        print(estado[-1])
-        return "deitado"'''
-    #Verificar se esta em pé baseando-se no quadril direito
-    if nariz.x >= quadril_direito.x-0.1 and nariz.x <= quadril_direito.x+0.1 and not(quadril_direito.y >= joelho_direito.y - 0.14 and quadril_direito.y <= joelho_direito.y + 0.14):
+    if anguloG > 70 and not(abs(centro_quadril.y-centro_joelho.y) <= 0.23):
         if primeiro_quadro:
-            tempo_validador = 60 
+            tempo_validador = tempo 
             primeiro_quadro = False     
         tempo_validador-=1  
         if tempo_validador <= 0:
             tempo_validador-=1
+            if estado[-1] != "de_pe":
+                print(estado[-1])
             estado.append("de_pe")
-            print(estado[-1])
-            tempo_validador = 60 
+            tempo_validador = tempo 
             primeiro_quadro = True        
-            return "de_pe"
-    #Verificar se esta em pé baseando-se no quadril esquerdo
-    '''elif nariz.x >= quadril_esquerdo.x-0.1 and nariz.x <= quadril_esquerdo.x+0.1 and estado[-1] != "de_pe" and not(quadril_esquerdo.y >= joelho_esquerdo.y - 0.14 and quadril_esquerdo.y <= joelho_esquerdo.y + 0.14):
-        estado.append("de_pe")
-        print(estado[-1])
-        return "de_pe"
-    #Verificar se esta sentado a partido do joelho direito
-    elif quadril_direito.y >= joelho_direito.y - 0.12 and quadril_direito.y <= joelho_direito.y + 0.12 and not(nariz.y >= quadril_direito.y-0.2 and nariz.y <= quadril_direito.y+0.2 and not(nariz.x >= quadril_direito.x-0.2 and nariz.x <= quadril_direito.x+0.2)) and estado[-1] != "sentado":
-            estado.append("sentado")
-            print(estado[-1])
-            return "sentado"
-    #Verificar se esta sentado a partido do joelho esquerdo
-    elif quadril_esquerdo.y >= joelho_esquerdo.y - 0.12 and quadril_esquerdo.y <= joelho_esquerdo.y + 0.12 and not(nariz.y >= quadril_esquerdo.y-0.2 and nariz.y <= quadril_esquerdo.y+0.2 and not(nariz.x >= quadril_esquerdo.x-0.2 and nariz.x <= quadril_esquerdo.x+0.2)) and estado[-1] != "sentado":
-            estado.append("sentado")
-            print(estado[-1])
-            return "sentado"'''
-    #print(estado[-1], "ESTADO")
+            return estado
+    #Verificar se esta sentado a partir do angulo do joelho e quadril central e tambem pela proximidade do jeelho central com o quadril central
+    if anguloJQ < 45 or abs(centro_quadril.y-centro_joelho.y) <= 0.23:
+            if primeiro_quadro:
+                tempo_validador = tempo 
+                primeiro_quadro = False     
+            tempo_validador-=1  
+            if tempo_validador <= 0:
+                if estado[-1] != "sentado":
+                    print(estado[-1])
+                estado.append("sentado")
+                tempo_validador = tempo 
+                primeiro_quadro = True
+                return estado
+    return estado
+
