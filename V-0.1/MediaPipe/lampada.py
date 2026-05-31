cf3a6d2df16324e67318241cf7392fd73e47a447
FelipeTxt <feliperemes1@gmail.com>
2026-05-31 15:03:44 -0300

    Refactoring the serverside, now, i'm gonna start making the pc control

--- V-0.1/MediaPipe/lampada.py
+++ V-0.1/MediaPipe/lampada.py
@@ -1,7 +1,7 @@
 import tinytuya
 
 lamp = tinytuya.Device(
-    dev_id='eb3709d3ce82370ee9xiqf',
+    dev_id='eb6cf639a85ba6907by7ss',
     address='192.168.0.101',
     local_key='65CC95DFEE111891',
     version=3.3
